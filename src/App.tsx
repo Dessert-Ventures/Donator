20e1dc6d171cd42e5b2a83c76e506f4454dcdd36
Orest Tarasiuk <orest@tarasiuk.me>
2021-10-25 16:14:22 +0200

    basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import logo from "./logo.svg"
 
 function App() {
+  // TODO: Prefill amount from URL param
+  const [amount, amountSetter] = useState<string>("10")
+
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newAmount: string = event.target.value
+    amountSetter(newAmount)
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    let amountAsNumber = parseInt(amount)
+    if (isNaN(amountAsNumber)) {
+      return
+    }
+
+    console.log(`Submitting donation request with amount ${amount}`)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {/* TODO: Logo & branding */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Donation Amount</p>
+            <input
+              type="number"
+              name="amount"
+              value={amount}
+              onChange={handleAmountChange}
+            />
+          </label>
+          <input type="submit" value="Donate" />
+        </form>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
