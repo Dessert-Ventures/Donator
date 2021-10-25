d3481474ef9ca90e39693650e0b75c7f9dd450e8
Orest Tarasiuk <orest@tarasiuk.me>
2021-10-25 16:26:30 +0200

    basic post request

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react"
+import { v4 as uuid } from "uuid"
 import "./App.css"
 import logo from "./logo.svg"
 
 function App() {
   // TODO: Prefill amount from URL param
   const [amount, amountSetter] = useState<string>("10")
+  const [email] = useState<string>("donator@dessertventures.com")
+  const [postError, postErrorSetter] = useState<string | undefined>()
 
   const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let newAmount: string = event.target.value
@@ -19,6 +22,26 @@ function App() {
     }
 
     console.log(`Submitting donation request with amount ${amount}`)
+    sendPostRequest(amountAsNumber, email)
+  }
+
+  const sendPostRequest = (amount: number, buyerEmail: string) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        amount: amount,
+        description: "Donation",
+        externalId: uuid(),
+        buyer: { email: buyerEmail },
+      }),
+    }
+    fetch("https://reqres.in/api/posts", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((e) => {
+        postErrorSetter(JSON.stringify(e))
+      })
   }
 
   return (
@@ -39,6 +62,8 @@ function App() {
           </label>
           <input type="submit" value="Donate" />
         </form>
+
+        {postError}
       </header>
     </div>
   )
